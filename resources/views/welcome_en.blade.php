ce0aa9696013a352dbcb51f1d9c5027c952a977a
KubQo0 <jakub13.danis@gmail.com>
2025-05-08 17:55:05 +0200

    nedokonceney nav bar5

--- resources/views/welcome_en.blade.php
+++ resources/views/welcome_en.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="text-center mt-20">
-        <h1 class="text-4xl font-bold mb-4">Welcome to the math site!</h1>
+        <h1 class="text-4xl font-bold mb-4 text-blue-600">Welcome to the math site!</h1>
         <p class="text-lg text-gray-700">Here you will find many math problems, exercises, and fun.</p>
     </div>
 @endsection
