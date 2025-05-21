f1b99527ce2d5e8c2404daa706839c6a43086ceb
D-Samuel <samzee.dutka@gmail.com>
2025-05-21 16:35:00 +0200

    added permission commands for docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,11 +7,18 @@ while ! mysqladmin ping -h mysql --silent; do
     sleep 1
 done
 
+# Fix permissions on the server
+echo "Setting correct permissions for storage and bootstrap/cache..."
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+# Install dependencies and build 
 npm install
 npm run build
 
+# Database setup + population
 echo "Running migrations and seeders..."
 php artisan migrate:fresh --seed --force
 
-# Then start PHP-FPM
+# Start PHP-FPM
 php-fpm
